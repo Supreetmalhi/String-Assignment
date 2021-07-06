4c4c0d2897296a200bfcd43335da5683f5347879
Supreetmalhi <malhisupreet40@gmail.com>
2021-07-05 23:20:10 -0400

    Initial commit

---
+++ String Assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace String_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a, b, c, d, e, f, g, h;
+            a = "My Name ";
+            b = "is ";
+            c = "Supreet.";
+            d = a + b + c;
+            Console.WriteLine(d);
+            Console.WriteLine(d.ToUpper());
+
+            e = " I am a student of Academy of Learning.";
+            f = " I am learning C# in this course.";
+            g = " Learning C# is fun.";
+
+            h = string.Concat(d, e, f, g);
+            Console.WriteLine(h);
+        }
+    }
+}
